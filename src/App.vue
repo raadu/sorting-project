d004ef8b830f55e8ae9b18bd6c9c8bbe4515f002
Raiyad Raad <raiyad.raad@sslwireless.com>
2025-01-29 13:05:48 +0600

    feature: modal CSS add

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,20 @@
 
     <div v-if="showModal" class="modal">
       <div class="modal-content">
-        <h3>How many people?</h3>
-        <p>Enter a number of how many people you want to add to the list</p>
+        <div class="modal-header">
 
-        <input type="number" v-model.number="numberOfPeople" min="20" max="100" required />
+          <h3 class="modal-title">How many people?</h3>
+          <button class="close-button" @click="showModal = false">&times;</button>
+        </div>
+        <p class="modal-body">Enter a number of how many people you want to add to the list.</p>
+
+        <div class="input-container">
+          <input type="number" v-model.number="numberOfPeople" min="20" max="100" required />
+        </div>
 
         <div class="modal-buttons">
-          <button @click="showModal = false">Cancel</button>
-          <button @click="handleStart">Start</button>
+          <button class="modal-button cancel" @click="showModal = false">Cancel</button>
+          <button class="modal-button start" @click="handleStart">Start</button>
         </div>
       </div>
     </div>
@@ -55,21 +61,87 @@ export default {
 
 .modal-content {
   background-color: white;
-  padding: 20px;
   border-radius: 5px;
   width: 300px;
 }
 
-.modal-buttons {
-  margin-top: 20px;
+.modal-header {
   display: flex;
-  gap: 10px;
-  justify-content: flex-end;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid grey;
+  padding: 8px;
 }
 
-input[type='number'] {
+.modal-title {
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.close-button {
+  background: none;
+  border: none;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  color: #333;
+}
+
+.close-button:hover {
+  color: #ff9800;
+}
+
+.modal-body {
+  padding: 12px 10px 0px 10px;
+  font-size: 12px;
+}
+
+.input-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+}
+
+.input-container input {
   width: 100%;
+  border: 1px solid grey;
+  border-radius: 4px;
+  outline: none;
   padding: 5px;
   margin: 10px 0;
 }
+
+.modal-buttons {
+  padding: 10px;
+  display: flex;
+  gap: 10px;
+  justify-content: flex-end;
+  border-top: 1px solid grey;
+}
+
+.modal-button {
+  padding: 8px 16px;
+  font-size: 14px;
+  font-weight: bold;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.modal-button.cancel {
+  background-color: #f5f5f5;
+  color: #333;
+}
+
+.modal-button.start {
+  background-color: #ff9800;
+  color: white;
+}
+
+/* input[type='number'] {
+  width: 100%;
+  padding: 5px;
+  margin: 10px 0;
+} */
 </style>
